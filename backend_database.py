f84ce7b67772cc6a41b1b0ad7e74e1de817bdc94
Yash Gupta <yash8740gupta@gmail.com>
2026-05-30 21:41:50 +0530

    Adding database backend

---
+++ backend_database.py
@@ -0,0 +1,43 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+# from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SQLiteSaver
+from langgraph.graph.message import add_messages
+import os
+from dotenv import load_dotenv
+import sqlite3
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0.7
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    reponse = llm.invoke(messages)
+    return {"messages": [reponse]}
+
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+checkpointer = SQLiteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
